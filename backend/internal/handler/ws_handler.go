3a06720ce312762e7904c6a914b4de531d7f31bd
Bantu-art <brianbaraza40@Gmail.com>
2025-05-10 16:34:04 +0300

    feat: implement ServeHTTP method for WebSocket connection handling and message processing

--- backend/internal/handler/ws_handler.go
+++ backend/internal/handler/ws_handler.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"log"
 	"net/http"
 	"real-time/backend/internal/repository"
 	"sync"
 
 	"github.com/gorilla/websocket"
+	"encoding/json"
 )
 
 var upgrader = websocket.Upgrader{
@@ -39,6 +41,47 @@ func NewWsHandler(userRepo repository.UserRepository) *WsHandler {
         register:   make(chan *websocket.Conn),
         unregister: make(chan *websocket.Conn),
         userRepo:   userRepo,
+    }
+}
+
+func (h *WsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    // Upgrade HTTP connection to WebSocket
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    // Handle connection
+    h.register <- conn
+    defer func() {
+        h.unregister <- conn
+        conn.Close()
+    }()
+
+    for {
+        messageType, message, err := conn.ReadMessage()
+        if err != nil {
+            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+                log.Printf("error: %v", err)
+            }
+            break
+        }
+
+        // Handle incoming message
+        var msg WsMessage
+        err = json.Unmarshal(message, &msg)
+        if err != nil {
+            log.Printf("error: %v", err)
+            continue
+        }
+
+        switch msg.Type {
+        case "send_message":
+            h.handleSendMessage(msg, conn)
+        case "get_messages":
+            h.handleGetMessages(msg, conn)
+        }
     }
 }
 
