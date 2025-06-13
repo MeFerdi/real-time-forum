3b120ff6147829b812b8eb6278f5a04b381250ef
MeFerdi <ferdinandodhis254@gmail.com>
2025-06-13 15:02:00 +0300

    refactor: remove CategoryDTO and its conversion method to simplify category model

--- backend/internal/model/category.go
+++ backend/internal/model/category.go
@@ -6,20 +6,6 @@ type Category struct {
 	Name string `json:"name" gorm:"unique;not null"`
 }
 
-// CategoryDTO is the data transfer object for Category
-type CategoryDTO struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-}
-
-// ToDTO converts Category to CategoryDTO
-func (c *Category) ToDTO() CategoryDTO {
-	return CategoryDTO{
-		ID:   c.ID,
-		Name: c.Name,
-	}
-}
-
 // PostCategory represents the many-to-many relationship between posts and categories
 type PostCategory struct {
 	PostID     int64 `json:"post_id" gorm:"primaryKey"`
