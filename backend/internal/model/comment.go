923abb49bbd0e5c1664961cae097ac5c97d66c35
MeFerdi <ferdinandodhis254@gmail.com>
2025-06-13 15:02:14 +0300

    refactor: remove CommentDTO and its conversion method to simplify comment model

--- backend/internal/model/comment.go
+++ backend/internal/model/comment.go
@@ -16,25 +16,3 @@ type Comment struct {
 	User User `gorm:"foreignKey:UserID"`
 	Post Post `gorm:"foreignKey:PostID"`
 }
-
-// CommentDTO is the data transfer object for Comment
-type CommentDTO struct {
-	ID        int       `json:"id"`
-	PostID    int       `json:"postId"`
-	UserID    int       `json:"userId"`
-	User      UserDTO   `json:"user"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"createdAt"`
-}
-
-// ToDTO converts Comment to CommentDTO
-func (c *Comment) ToDTO(user UserDTO) CommentDTO {
-	return CommentDTO{
-		ID:        c.ID,
-		PostID:    c.PostID,
-		UserID:    c.UserID,
-		User:      user,
-		Content:   c.Content,
-		CreatedAt: c.CreatedAt,
-	}
-}
